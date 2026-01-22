40e609982135ce65db4533ac0c308594f373c3dc
NewSunH <NewSunH@bjmu.edu.cn>
2026-01-22 15:24:19 +0800

    feat: new Linear class

--- tests/adapters.py
+++ tests/adapters.py
@@ -29,7 +29,11 @@ def run_linear(
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
 
-    raise NotImplementedError
+    from cs336_basics.linear import Linear
+
+    linear = Linear(d_in=d_in, d_out=d_out)
+    linear.load_state_dict({"weights": weights})
+    return linear.forward(in_features)
 
 
 def run_embedding(
